bbc686485cfa52fd6d070db90c93c11ca81cbd34
Sagar Grade <sagar.grade@colruytgroup.com>
2024-07-15 13:47:57 +0200

    Replaced OpenFeign to RestClient for communication b/w services

--- api/order-service/src/main/java/edu/mylearning/microservices/order/client/InventoryClient.java
+++ api/order-service/src/main/java/edu/mylearning/microservices/order/client/InventoryClient.java
@@ -1,12 +1,12 @@
 package edu.mylearning.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${application.inventory.service.url}")
+//@FeignClient(value = "inventory", url = "${application.inventory.service.url}")
 public interface InventoryClient {
 
-    @GetMapping(value = "/api/inventory")
+//    @GetMapping(value = "/api/inventory")
+    @GetExchange(value = "/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
