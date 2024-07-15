820251e63aa43d0d63aca221e3a46aebff68dc6c
Sagar Grade <sagar.grade@colruytgroup.com>
2024-07-15 14:39:38 +0200

    Added CircuitBreaker in OrderService

--- api/order-service/src/main/java/edu/mylearning/microservices/order/client/InventoryClient.java
+++ api/order-service/src/main/java/edu/mylearning/microservices/order/client/InventoryClient.java
@@ -1,12 +1,25 @@
 package edu.mylearning.microservices.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 //@FeignClient(value = "inventory", url = "${application.inventory.service.url}")
 public interface InventoryClient {
 
-//    @GetMapping(value = "/api/inventory")
+    static final Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
+    //    @GetMapping(value = "/api/inventory")
     @GetExchange(value = "/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Can not get inventory for sku code {} failure reason : {}", code, throwable.getMessage());
+        return false;
+    }
 }
